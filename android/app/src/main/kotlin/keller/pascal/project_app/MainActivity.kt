e747019d3af65e0c9c7554764ba64ecf5598cc7c
Keller <pasckell01@gmail.com>
2026-04-23 09:45:23 +0200

    Initialer Commit

---
+++ android/app/src/main/kotlin/keller/pascal/project_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package keller.pascal.project_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    includeBuild("flutter_tools_gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
+include(":app")
